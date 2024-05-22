18da8ab3f9bc1d066488f55623942246bddf2f1a
farkuy <darkuy@mail.ru>
2024-05-22 23:34:30 +0300

    довел связь авторизации с бэком до ума

--- src/http/index.ts
+++ src/http/index.ts
@@ -9,7 +9,7 @@ export const $authHost = axios.create({
 })
 
 const authInterceptor = (config: any) => {
-    config.headers.authorizationz = `Bearer ${localStorage.getItem('token')}`
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
     return config
 }
 
--- src/http/userApi.ts
+++ src/http/userApi.ts
@@ -6,19 +6,24 @@ export const login = async (email: string, password: string) => {
         email, password
     });
     localStorage.setItem('token', data.token)
-    return jwtDecode(data);
+    return jwtDecode(data.token);
 }
 
 export const registration = async (email: string, password: string) => {
     const { data } = await $host.post('auth/registration', {
         email, password
     });
+    console.log(data)
     localStorage.setItem('token', data.token)
-    return jwtDecode(data);
+    return jwtDecode(data.token);
 }
 
-export const checkAuth = async () => {
-    const { data } = await $authHost.get('auth/checkAuth');
+export const checkAuth = async (jwt: string) => {
+    const { data } = await $authHost.get('auth/checkAuth', {
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        }
+    });
     localStorage.setItem('token', data.token);
     return jwtDecode(data.token);
 }
